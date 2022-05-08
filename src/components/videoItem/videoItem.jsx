471d2deee2608fa63e77fb1864a58caadce7fb96
tldnjs2213 <tldnjs2213@naver.com>
2022-05-08 22:58:27 +0900

    make video item

--- src/components/videoItem/videoItem.jsx
+++ src/components/videoItem/videoItem.jsx
@@ -1,5 +1,30 @@
 import React from 'react';
+import styles from './videoItem.module.css';
 
-const VideoItem = (props) => <h1>{props.video.snippet.title}</h1>;
+// const VideoItem = (props) => (
+// 	<li>
+// 		<img src={props.video.snippet.thumbnails.medium.url} alt="video thumbnail" />
+//     <div>
+//       <p>{props.video.snippet.title}</p>
+//       <p>{props.video.snippet.channelTitle}</p>
+//     </div>
+// 	</li>
+// );
+
+// export default VideoItem;
+
+// -> deconstructing
+
+const VideoItem = ({ video: { snippet } }) => (
+	<li className={styles.container}>
+		<div className={styles.video}>
+			<img className={styles.thumbnail} src={snippet.thumbnails.medium.url} alt="video thumbnail" />
+			<div className={styles.metadata}>
+				<p className={styles.title}>{snippet.title}</p>
+				<p className={styles.channel}>{snippet.channelTitle}</p>
+			</div>
+		</div>
+	</li>
+);
 
 export default VideoItem;
--- src/components/videoList/videoList.jsx
+++ src/components/videoList/videoList.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import VideoItem from '../videoItem/videoItem';
+import styles from './videoList.module.css'
 
 const VideoList = (props) => (
-	<ul>
+	<ul className={styles.videos}>
 		{props.videos.map((video) => (
 			<VideoItem key={video.id} video={video} />
 		))}
